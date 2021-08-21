8c48ac3f975cb458d36614d590e110934b254d20
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-08-21 11:48:52 +0530

    Add files via upload

---
+++ Student_Project.py
@@ -0,0 +1,20 @@
+import turtle
+
+
+turtle.width(4)
+turtle.penup()
+
+turtle.pendown()
+
+
+for i in range():
+    
+    
+    turtle.right(120)
+    turtle.forward(130)
+    turtle.right(120)
+    turtle.forward(130)
+    turtle.right(120)
+    turtle.forward(130)
+    turtle.right(72)
+    
